236995e3436e9614857df28448ba7282b03032e2
FrancisGarryNillama <paenggwapokaayo123@gmail.com>
2025-11-29 05:15:43 +0800

    Refractored frontend

--- django_react_credit/backend/myApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterUser, LoginUser, login_view
-
-urlpatterns = [
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('login/', login_view, name='login'),
-]
--- django_react_credit/backend/torchecker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/preview/', views.upload_preview),
-    path('upload/full/', views.upload_full),
-]
