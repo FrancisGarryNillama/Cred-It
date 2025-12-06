849855c37409d54c5c4871bbcdea22d85d990134
JG, Rosarito <120697604+IfeelPa1n@users.noreply.github.com>
2025-12-06 21:55:33 +0800

    Delete torchecker directory
    incorrect push

--- torchecker/apps.py
+++
@@ -1,8 +0,0 @@
-"""App configuration for torchecker"""
-from django.apps import AppConfig
-
-
-class TorcheckerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'torchecker'
-    verbose_name = 'TOR Checker'
--- torchecker/services/__init__.py
+++
@@ -1,5 +0,0 @@
-"""TorChecker services package"""
-from .ocr_service import OCRService
-from .tor_service import TorService
-
-__all__ = ['OCRService', 'TorService']
